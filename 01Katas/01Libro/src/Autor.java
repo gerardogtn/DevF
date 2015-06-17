7a105cc1ae4317fca483c673aa5fde2609824797
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-17 10:58:40 -0500

    Add Autor attributes and constructor'

--- 01Katas/01Libro/src/Autor.java
+++ 01Katas/01Libro/src/Autor.java
@@ -2,4 +2,14 @@
  * Created by gerardogtn on 6/16/15.
  */
 public class Autor {
+
+    private String nombre;
+    private String email;
+    private char genero;
+
+    public Autor(String name, String email, char gender){
+        this.nombre = name;
+        this.email = email;
+        this.genero = gender;
+    }
 }
