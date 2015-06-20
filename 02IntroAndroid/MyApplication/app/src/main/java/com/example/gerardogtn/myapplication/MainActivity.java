3a4069508cc523053c29f41fa57d5331adb79394
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-19 22:04:00 -0500

    Add overriden activity flow methods

--- 02IntroAndroid/MyApplication/app/src/main/java/com/example/gerardogtn/myapplication/MainActivity.java
+++ 02IntroAndroid/MyApplication/app/src/main/java/com/example/gerardogtn/myapplication/MainActivity.java
@@ -1,17 +1,48 @@
 package com.example.gerardogtn.myapplication;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = AppCompatActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 
     @Override
