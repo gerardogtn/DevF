9e798f0be35d4c769ee5196f14a53b7d3a7f83b1
Gerardo Teruel <gerardogtn@gmail.com>
2015-06-29 11:46:23 -0500

    Add UserProfileActivity

---
+++ 02IntroAndroid/LayoutExamples/app/src/main/java/com/example/gerardogtn/layoutexamples/UserProfileActivity.java
@@ -0,0 +1,20 @@
+package com.example.gerardogtn.layoutexamples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gerardogtn on 6/28/15.
+ */
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
