21493c90ff50ae24d046b27584686a7f126656f4
Gerardo Teruel <gerardogtn@gmail.com>
2015-07-01 21:19:20 -0500

    Implement StaticFragment interface for interfragment communication

--- 02IntroAndroid/IntroFragments/app/src/main/java/com/example/gerardogtn/introfragments/MainActivity.java
+++ 02IntroAndroid/IntroFragments/app/src/main/java/com/example/gerardogtn/introfragments/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.gerardogtn.introfragments;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.gerardogtn.introfragments.fragment.DynamicFragment;
+import com.example.gerardogtn.introfragments.fragment.StaticFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements StaticFragment.OnPokemonSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addDynamicFragment(){
+        addDynamicFragment("", -1);
+    }
+
+    private void addDynamicFragment(String name, int atk){
         FragmentTransaction tm = getSupportFragmentManager().beginTransaction();
-        tm.add(R.id.dynamic_fragment, DynamicFragment.getInstance("Charizard", 23));
+        tm.replace(R.id.dynamic_fragment, DynamicFragment.getInstance(name, atk));
         tm.commit();
     }
 
+
+    @Override
+    public void onPokemonSelected(String name, int atk) {
+        addDynamicFragment(name, atk);
+    }
 }
