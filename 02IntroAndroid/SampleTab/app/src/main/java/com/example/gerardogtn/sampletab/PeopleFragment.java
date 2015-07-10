6f67468d6217dc98c0223e6bac9690fcf0489890
Gerardo Teruel <gerardogtn@gmail.com>
2015-07-10 16:47:50 -0500

    Implement ListView of people

--- 02IntroAndroid/SampleTab/app/src/main/java/com/example/gerardogtn/sampletab/PeopleFragment.java
+++ 02IntroAndroid/SampleTab/app/src/main/java/com/example/gerardogtn/sampletab/PeopleFragment.java
@@ -1,18 +1,38 @@
 package com.example.gerardogtn.sampletab;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class PeopleFragment extends Fragment {
 
+    private static List<PersonModel> people;
+
+    @Bind(R.id.list_people)
+    ListView listView;
 
     public PeopleFragment() {
-
+        people = new ArrayList<>();
+        people.add(new PersonModel("Chava", R.drawable.chava, true));
+        people.add(new PersonModel("Petter", R.drawable.petter, true));
+        people.add(new PersonModel("Angel", R.drawable.angel));
+        people.add(new PersonModel("Emilio", R.drawable.emilio));
+        people.add(new PersonModel("Fer", R.drawable.fer));
+        people.add(new PersonModel("Gerardo", R.drawable.gerardo));
+        people.add(new PersonModel("Salvador", R.drawable.salvador));
+        people.add(new PersonModel("Sebastian", R.drawable.sebastian));
     }
 
     public static PeopleFragment newInstance(){
@@ -21,9 +41,24 @@ public class PeopleFragment extends Fragment {
     }
 
     @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_people, container, false);
+        View root =  inflater.inflate(R.layout.fragment_people, container, false);
+        ButterKnife.bind(this, root);
+
+        PersonAdapter adapter = new PersonAdapter(getActivity(), 0, people);
+        listView.setAdapter(adapter);
+        return root;
     }
 
 
